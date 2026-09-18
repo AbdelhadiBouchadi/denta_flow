302e782305917deb65e692c19de16aab3d2a8789
Abdelhadi Bouchadi <2110213582@ogrenci.karabuk.edu.tr>
2026-09-18 15:42:38 +0100

    feat: scaffold

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+export const DEFAULT_PAGE = 1;
+export const DEFAULT_PAGE_SIZE = 10;
+export const MAX_PAGE_SIZE = 100;
+export const MIN_PAGE_SIZE = 1;
+
+export const CLINIC_TIMEZONE = "Africa/Casablanca";
+export const CURRENCY_SUFFIX = "DH";
+
+export const WEEK_STARTS_ON = 1; // lundi (ISO)
+export const AGENDA_SLOT_MINUTES = 15;
+export const AGENDA_DAY_START = "08:00";
+export const AGENDA_DAY_END = "20:00";
